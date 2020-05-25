382075d534a085b544038418d6eb3b04ea2910b1
Viacheslav Burlaka <viacheslav.burlaka@uvoteam.com>
2020-05-25 15:26:06 +0300

    01 init project; added materialUI, Router, scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from "./components/Navbar";
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// Pages
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <div className="Container">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={Signup}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink} from "react-router-dom";
+
+// Material UI
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar className="Navbar__container">
+          <Button color="inherit" component={NavLink} to="/">Home</Button>
+          <Button color="inherit" component={NavLink} to="/login">Login</Button>
+          <Button color="inherit" component={NavLink} to="/signup">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
